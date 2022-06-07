fb6dd2e5252be927709e4002feae9e8494219326
SOG-web <raheemolalekanusman84@gmail.com>
2022-06-07 15:04:53 +0100

    2 files modified, 3 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import * as React from "react";
+import * as React from 'react';
+import { Outlet, NavLink } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              display: 'inline-block',
+              margin: '1rem 0',
+              color: isActive ? 'red' : '',
+            };
+          }}
+          to="/invoices"
+        >
+          Invoices
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              display: 'inline-block',
+              margin: '1rem 20px',
+              color: isActive ? 'red' : '',
+            };
+          }}
+          to="/expenses"
+        >
+          Expenses
+        </NavLink>
+      </nav>
+      <Outlet />
     </div>
   );
 }
